5c12a063b83e5fdc218c1a90802ab8ea907cd663
sahar sakhaiee <100339904+shrsakhaiee@users.noreply.github.com>
2022-05-18 14:03:42 +0430

    update!!

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,8 @@ namespace _2048
     {
         Label[,] game_board;
         int n = 4;
+        int r_x, r_y;
+        string r;
 
         public Form1()
         {
@@ -38,104 +40,108 @@ namespace _2048
 
                 }
             }
-
-            Random r = new Random();
-            int r1, r2, r1_x, r1_y, r2_x, r2_y,random_index;
-            int[] init_numbers = { 2, 2, 2, 2, 2, 4 };
-
-            random_index = r.Next(0, init_numbers.Length);
-            r1 = init_numbers[random_index];
-
-            random_index = r.Next(0, init_numbers.Length);
-            r2 = init_numbers[random_index];
-
-            r1_x = r.Next(0, 3);
-            r1_y = r.Next(0, 3);
-            do
-            {
-                r2_x = r.Next(0, 3);
-                r2_y = r.Next(0, 3);
-            } while (r1_x == r2_x && r1_y == r2_y);
-
-            game_board[r1_x, r1_y].Text = Convert.ToString(r1);
-            game_board[r2_x, r2_y].Text = Convert.ToString(r2);
-
+            make_Rn();
+            make_Rn();
         }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyData == Keys.Left)
+            bool move = false;
+            for (int k = 0; k < 3; k++)
             {
-                for (int k = 0; k < 3; k++)
+                for (int i = 0; i < n; i++)
                 {
-                    for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
                     {
-                        for (int j = 0; j < n; j++)
-                        {
-                            if (i > 0 && game_board[i - 1 , j].Text == "")
+                        if (game_board[i, j].Text != "")
+                            if (e.KeyData == Keys.Left)
                             {
-                                game_board[i - 1 , j].Text = game_board[i, j].Text;
-                                game_board[i, j].Text = "";
 
+                                if (i > 0 && game_board[i - 1, j].Text == "")
+                                {
+                                    move = true;
+                                    game_board[i - 1, j].Text = game_board[i, j].Text;
+                                    game_board[i, j].Text = "";
+
+                                }
+                                else if (j > 0 && game_board[i - 1, j].Text == game_board[i, j].Text)
+                                {
+                                    game_board[i - 1, j].Text = Convert.ToString(Convert.ToInt32(game_board[i, j].Text) * 2);
+                                    game_board[i, j].Text = "";
+                                }
+                                else if (e.KeyData == Keys.Right)
+                                {
+                                    if (i < n - 1 && game_board[i + 1, j].Text == "")
+                                    {
+                                        move = true;
+                                        game_board[i + 1, j].Text = game_board[i, j].Text;
+                                        game_board[i, j].Text = "";
+
+                                    }
+                                    else if (j > n - 1 && game_board[i + 1, j].Text == game_board[i, j].Text)
+                                    {
+                                        game_board[i + 1, j].Text = Convert.ToString(Convert.ToInt32(game_board[i, j].Text) * 2);
+                                        game_board[i, j].Text = "";
+                                    }
+                                    else if (e.KeyData == Keys.Down)
+                                    {
+                                        if (j < n - 1 && game_board[i, j + 1].Text == "")
+                                        {
+                                            move = true;
+                                            game_board[i, j + 1].Text = game_board[i, j].Text;
+                                            game_board[i, j].Text = "";
+
+                                        }
+                                        else if (j < n - 1 && game_board[i, j + 1].Text == game_board[i, j].Text)
+                                        {
+                                            game_board[i, j + 1].Text = Convert.ToString(Convert.ToInt32(game_board[i, j].Text) * 2);
+                                            game_board[i, j].Text = "";
+                                        }
+                                        else if (e.KeyData == Keys.Up)
+                                        {
+                                            if (j > 0 && game_board[i, j - 1].Text == "")
+                                            {
+                                                move = true;
+                                                game_board[i, j - 1].Text = game_board[i, j].Text;
+                                                game_board[i, j].Text = "";
+
+                                            }
+                                            else if (j > 0 && game_board[i, j - 1].Text == game_board[i, j].Text)
+                                            {
+                                                game_board[i, j - 1].Text = Convert.ToString(Convert.ToInt32(game_board[i, j].Text) * 2);
+                                                game_board[i, j].Text = "";
+                                            }
+                                        }
+                                    }
+                                }
                             }
-                        }
                     }
-                }
-            }
-            else if(e.KeyData==Keys.Right)
-            {
-                for (int k = 0; k < 3; k++)
-                {
-                    for (int i = 0; i < n; i++)
+                    if (move == true)
                     {
-                        for (int j = 0; j < n; j++)
-                        {
-                            if (i < n - 1 && game_board[i + 1 , j ].Text == "")
-                            {
-                                game_board[i + 1 , j].Text = game_board[i, j].Text;
-                                game_board[i, j].Text = "";
 
-                            }
-                        }
-                    }
-                }
-            }
-            else if(e.KeyData==Keys.Down)
-            {
-                for (int k = 0; k < 3; k++)
-                {
-                    for (int i = 0; i < n; i++)
-                    {
-                        for (int j = 0; j < n; j++)
-                        {
-                            if (j < n - 1 && game_board[i , j + 1].Text == "")
-                            {
-                                game_board[i , j + 1].Text = game_board[i, j].Text;
-                                game_board[i, j].Text = "";
-
-                            }
-                        }
-                    }
-                }
-            }
-            else if(e.KeyData == Keys.Up)
-            {
-                for(int k=0;k<3;k++)
-                { 
-                    for(int i=0;i<n;i++)
-                    {
-                        for (int j = 0; j < n; j++)
-                        {
-                            if (j > 0 && game_board[i , j - 1].Text == "")
-                            {
-                                game_board[i , j - 1].Text = game_board[i, j].Text;
-                                game_board[i, j].Text = "";
-
-                            }
-                        }
+                        make_Rn();
                     }
                 }
             }
         }
-            
-    }
-}
+                    private void make_Rn()
+                    {
+                        var Rand = new Random();
+                        int[] num = { 2, 2, 2, 4 };
+                        bool empty;
+
+                        do
+                        {
+                            empty = true;
+                            r_x = Rand.Next(0, n - 1);
+                            r_y = Rand.Next(0, n - 1);
+
+                            if (game_board[r_x, r_y].Text != "")
+                                empty = false;
+                        } while (empty == false);
+
+                        r = (num[Rand.Next(0, num.Length)]).ToString();
+                        game_board[r_x, r_y].Text = r;
+                    }
+                }
+            }
+
